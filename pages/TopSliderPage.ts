0d1787e819f6d8541e2f97282de5dcc230046a46
Karine <karinegasesyan@gmail.com>
2026-02-17 23:30:49 +0400

    fix: Top slider functonality

--- pages/TopSliderPage.ts
+++ pages/TopSliderPage.ts
@@ -10,72 +10,73 @@ export class TopSlider {
   async expectSliderVisible() {
     await expect(this.page.locator(".swiper-viewport").first()).toBeVisible();
   }
+
   async clickNextWithCheck() {
-    if (
-      await this.page.getByRole("link", { name: "iPhone" }).nth(1).isVisible()
-    ) {
-      await this.page.locator(".swiper-button-next").first().click();
-      await expect(
-        this.page.getByRole("img", { name: "MacBookAir" }).nth(1),
-      ).toBeVisible();
-    } else {
-      await this.page.locator(".swiper-button-next").first().click();
-      await expect(
-        this.page.getByRole("link", { name: "iPhone" }).nth(1),
-      ).toBeVisible();
-    }
+    await this.page.locator(".swiper-button-next").first().click();
+    await expect(
+      this.page
+        .locator(".swiper-viewport")
+        .first()
+        .getByRole("img", { name: /MacBookAir/i })
+        .first(),
+    ).toBeVisible();
   }
+
   async clickPrevWithCheck() {
-    if (
-      await this.page
-        .getByRole("link", { name: "MacBookAir" })
-        .nth(1)
-        .isVisible()
-    ) {
-      await this.page.locator(".swiper-button-prev").first().click();
-      await expect(
-        this.page.getByRole("link", { name: "iPhone" }).nth(1),
-      ).toBeVisible();
-    } else {
-      await this.page.locator(".swiper-button-prev").first().click();
-      await expect(
-        this.page.getByRole("img", { name: "MacBookAir" }).nth(1),
-      ).toBeVisible();
-    }
+    await this.page.locator(".swiper-button-prev").first().click();
+    await expect(
+      this.page
+        .locator(".swiper-viewport")
+        .first()
+        .getByRole("link", { name: /iPhone/i })
+        .first(),
+    ).toBeVisible();
   }
+
   async clickDotAndCheck() {
     const slider = this.page.locator(".swiper-viewport").first();
     const dots = this.page.locator(".swiper-pagination-bullet");
+
     await dots.nth(0).click();
     await expect(dots.nth(0)).toHaveClass(/swiper-pagination-bullet-active/);
     await expect(
       slider.getByRole("link", { name: /iPhone/i }).first(),
     ).toBeVisible();
+
     await dots.nth(1).click();
     await expect(dots.nth(1)).toHaveClass(/swiper-pagination-bullet-active/);
     await expect(
-      this.page.getByRole("img", { name: "MacBookAir" }).nth(1),
+      slider.getByRole("img", { name: /MacBookAir/i }).first(),
     ).toBeVisible();
   }
+
   async testFirstSliderImage() {
-    await this.page.getByRole("link", { name: "iPhone" }).first().click();
-    await expect(this.page).toHaveTitle("Samsung Galaxy Tab 10.1");
-    await expect(this.page).toHaveURL(
-      "https://tutorialsninja.com/demo/index.php?route=product/product&path=57&product_id=49",
-    );
+    await this.page
+      .getByRole("link", { name: /iPhone/i })
+      .first()
+      .click();
+
+    await expect(this.page).toHaveTitle(/Samsung Galaxy Tab/);
+    await expect(this.page).toHaveURL(/product_id=49/);
+
     await expect(
-      this.page.getByRole("link", { name: "Samsung Galaxy Tab" }).nth(1),
+      this.page.getByRole("link", { name: /Samsung Galaxy Tab/i }).nth(1),
     ).toBeVisible();
+
     await expect(
-      this.page.getByRole("heading", { name: "Samsung Galaxy Tab" }),
+      this.page.getByRole("heading", { name: /Samsung Galaxy Tab/i }),
     ).toBeVisible();
+
     await expect(this.page.getByText("Product Code:SAM1")).toBeVisible();
     await expect(this.page.getByText("Reward Points:")).toBeVisible();
     await expect(this.page.getByText("Availability:Pre-Order")).toBeVisible();
     await expect(this.page.getByText("Ex Tax:$")).toBeVisible();
     await expect(this.page.getByText("Qty")).toBeVisible();
+
     await expect(this.page.getByRole("textbox", { name: "Qty" })).toBeVisible();
-    await this.page.getByRole("button", { name: "Add to Cart" }).click();
-    await expect(this.page.getByText("Success: You have added")).toBeVisible();
+
+    await this.page.getByRole("button", { name: /Add to Cart/i }).click();
+
+    await expect(this.page.getByText(/Success: You have added/i)).toBeVisible();
   }
 }
--- tests/top-slider.spec.ts
+++ tests/top-slider.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from "@playwright/test";
+import { test } from "@playwright/test";
 import { TopSlider } from "../pages/TopSliderPage";
 import { BasePage } from "../pages/BasePage";
 
@@ -11,24 +11,24 @@ test.beforeEach(async ({ page }) => {
   await basePage.open();
 });
 
-test.describe("Testing top slider", () => {
-  test("Test if slider is visible", async ({ page }) => {
+test.describe.only("Testing top slider", () => {
+  test("Test if slider is visible", async () => {
     await topSlider.expectSliderVisible();
   });
 
-  test("Test slider Next button functionality", async ({ page }) => {
+  test("Test slider Next button functionality", async () => {
     await topSlider.clickNextWithCheck();
   });
 
-  test("Test slider Previous button functionality", async ({ page }) => {
+  test("Test slider Previous button functionality", async () => {
     await topSlider.clickPrevWithCheck();
   });
 
-  test("Test slider indicators/dots", async ({ page }) => {
+  test("Test slider indicators/dots", async () => {
     await topSlider.clickDotAndCheck();
   });
 
-  test("Test first slider image link", async ({ page }) => {
+  test("Test first slider image link", async () => {
     await topSlider.testFirstSliderImage();
   });
 });
